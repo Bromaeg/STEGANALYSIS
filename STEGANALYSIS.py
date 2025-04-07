13be2bdc6ff3a441c6fb4d4f6a00deba0cf7494a
Bromaeg <miketena1711@icloud.com>
2025-04-06 20:37:28 -0600

    Eliminacion de comentarios

--- STEGANALYSIS.py
+++ STEGANALYSIS.py
@@ -59,21 +59,25 @@ train_val_paths, test_paths, train_val_labels, test_labels = train_test_split(
 train_paths, val_paths, train_labels, val_labels = train_test_split(
     train_val_paths, train_val_labels, test_size=0.20, random_state=42, stratify=train_val_labels)
 
+# Validacion de separacion de datos
 print(f"Conjunto de imágenes:\n  Entrenamiento: {len(train_paths)}\n  Validación: {len(val_paths)}\n  Prueba: {len(test_paths)}")
 
 # Pipeline de datos
 
 def load_image(filename):
+
     # Carga la imagen sin redimensionarla para preservar las sutilezas de la esteganografía.
     image_string = tf.io.read_file(filename)
     image = tf.image.decode_jpeg(image_string, channels=3)
     image = tf.cast(image, tf.float32)
+
     # Normalización: escalar a [0, 1] preservando las relaciones entre valores.
     image /= 255.0
     return image
 
 def create_dataset(paths, labels, batch_size=8, shuffle=False, shuffle_buffer=1000):
     ds = tf.data.Dataset.from_tensor_slices((paths, labels))
+
     # Cargamos la imagen y conservamos la ruta (si la necesitas para otras verificaciones)
     ds = ds.map(lambda x, y: (load_image(x), y, x), num_parallel_calls=tf.data.AUTOTUNE)
     if shuffle:
@@ -88,14 +92,13 @@ train_ds = create_dataset(train_paths, train_labels, batch_size, shuffle=True, s
 val_ds = create_dataset(val_paths, val_labels, batch_size, shuffle=False)
 test_ds = create_dataset(test_paths, test_labels, batch_size, shuffle=False)
 
-# Ejemplo: mostrar la forma de un lote del conjunto de entrenamiento
+# DEV mostrar la forma de un lote del conjunto de entrenamiento
 for images, labels, paths in train_ds.take(1):
     print("Forma del batch de imágenes:", images.shape)
     print("Forma del batch de etiquetas:", labels.shape)
     print("Batch de rutas:", paths.shape)
 
-# --- Prueba con una imagen aleatoria del entrenamiento ---
-
+# Prueba con una imagen aleatoria del entrenamiento 
 for images, labels, paths in train_ds.take(1):
     idx = random.randint(0, images.shape[0] - 1)
     chosen_image = images[idx]
@@ -106,7 +109,7 @@ for images, labels, paths in train_ds.take(1):
     print("Etiqueta asignada:", chosen_label)
     print("Ruta del archivo:", chosen_path)
 
-    # Aquí solo mostramos el label sin comparar con la carpeta de origen.
+    # Mostramos el label
     plt.imshow((chosen_image.numpy() * 255).astype("uint8"))
     plt.title(f"Etiqueta: {chosen_label} | Archivo: {os.path.basename(chosen_path)}")
     plt.axis("off")
